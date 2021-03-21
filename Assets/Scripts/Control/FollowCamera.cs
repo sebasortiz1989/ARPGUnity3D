fe5f51f2deb63c2b8b61ed30079c2ead519669c1
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-03-21 11:01:50 -0500

    Basic Movement Tweaks

--- Assets/Scripts/Control/FollowCamera.cs
+++ Assets/Scripts/Control/FollowCamera.cs
@@ -8,7 +8,7 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] Transform target;
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         this.transform.position = target.position;
     }
