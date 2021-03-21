e73c05692565fe37891e7693bae32c85c5200fa9
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-03-21 06:30:52 -0500

    Implement Click-To-Move

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,12 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     // Config
-    [SerializeField] Transform target;
 
     // Cached Component References
     NavMeshAgent playerNavMeshAgent;
 
     // Initialize Variables
-    Ray lastRay;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +23,20 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MoveToCursor();
+    }
+
+    private void MoveToCursor()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInformation;
+            bool hasHit = Physics.Raycast(ray, out hitInformation);
+            if (hasHit)
+            {
+                playerNavMeshAgent.destination = hitInformation.point;
+            }
         }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100, Color.red);
-        playerNavMeshAgent.destination = target.transform.position;
     }
 }
