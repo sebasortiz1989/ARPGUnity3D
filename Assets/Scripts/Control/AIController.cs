af6d3b91903f6bc2a82228688135773740e2fefa
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-03-25 19:18:07 -0500

    That's some suspicious behaviour

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
+using System;
 
 namespace RPG.Control
 {
@@ -10,6 +13,7 @@ namespace RPG.Control
     {
         // Config
         [SerializeField] float chaseDistance = 8f;
+        [SerializeField] float timeToWaitSuspiciously = 6f;
 
         // String const
         private const string PLAYER_TAG = "Player";
@@ -18,16 +22,27 @@ namespace RPG.Control
         GameObject player;
         Health health;
         Fighter fighter;
+        Mover mover;
+        NavMeshAgent navMeshAgent;
+        ActionScheduler actionScheduler;
 
         // Initialize Variables
         float distanceToPlayer;
-
+        Vector3 guardLocation;
+        Quaternion guardRotation;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
+        
         // Start is called before the first frame update
         void Start()
         {
             player = GameObject.FindWithTag(PLAYER_TAG);
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            actionScheduler = GetComponent<ActionScheduler>();
+            guardLocation = transform.position;
+            guardRotation = transform.rotation;
         }
 
         // Update is called once per frame
@@ -42,19 +57,46 @@ namespace RPG.Control
             distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
             if (distanceToPlayer <= chaseDistance && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
+                timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
+            }
+            else if (timeSinceLastSawPlayer < timeToWaitSuspiciously)
+            {
+                SuspicionBehaviour();
             }
             else
             {
-                fighter.Cancel();
+                GuardBehaviour();
             }
+
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void GuardBehaviour()
+        {
+            fighter.Cancel();
+            mover.StartMoveAction(guardLocation);
+            if (navMeshAgent.velocity.magnitude < 0.05f && transform.rotation != guardRotation)
+            {
+                transform.rotation = guardRotation;
+            }
+        }
+
+        private void SuspicionBehaviour()
+        {
+            actionScheduler.CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
         }
 
         // Called by Unity
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.blue;
-            Gizmos.DrawWireSphere(transform.position, chaseDistance / 2);
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
         }
     }
 }
