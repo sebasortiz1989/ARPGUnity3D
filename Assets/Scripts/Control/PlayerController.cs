f107242010a0623849536abfeae06284ae0a5ee6
Juan@SDSharp <juan@sdsharp.com>
2021-06-09 07:06:21 -0500

    Calculating A NavMesh Path

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -14,6 +14,7 @@ namespace RPG.Control
     {
         [SerializeField] CursorMapping[] _cursorMappings = null;
         [SerializeField] float maxNavMeshProjectionDistance = 1f;
+        [SerializeField] float maxNavPathLength = 40f;
 
         [System.Serializable]
         struct CursorMapping
@@ -137,7 +138,28 @@ namespace RPG.Control
             if (!hasCastToNavMesh) return false;
 
             target = navMeshHit.position;
+
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+            if (!hasPath) return false;
+            if (path.status != NavMeshPathStatus.PathComplete) return false;
+            if (GetPathLength(path) > maxNavPathLength) return false;
+
             return true;
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            float total = 0;
+            if (path.corners.Length < 2) return total;
+
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                float distance = Vector3.Distance(path.corners[i], path.corners[i + 1]);
+                total += distance;
+            }
+                   
+            return total;
         }
 
         private static Ray GetMouseRay()
